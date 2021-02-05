ee40d1f208422eed9bcbe4a0d0d3d8c31d9f4b4c
Kinza Drewett <kinza.drewett@redington.co.uk>
2021-02-05 17:17:19 +0000

    consoleapp now calls RouteFinder and takes in user input

---
+++ RouteFinder.ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RouteFinder.ConsoleApp
+{
+    using System.Collections.Generic;
+    using Core;
+
+    class Program
+    {
+        static void Main()
+        {
+            var paths = new List<Path>
+            {
+                new Path {Id = 1, PointOne = Points.A, PointTwo = Points.D, Distance = 20},
+                new Path {Id = 2, PointOne = Points.D, PointTwo = Points.A, Distance = 14},
+                new Path {Id = 3, PointOne = Points.A, PointTwo = Points.C, Distance = 15},
+                new Path {Id = 4, PointOne = Points.A, PointTwo = Points.B, Distance = 5},
+                new Path {Id = 5, PointOne = Points.B, PointTwo = Points.C, Distance = 5}
+            };
+
+            Console.WriteLine("Welcome to my application! I will calculate the shortest route if you give me a start and end point.");
+            Console.WriteLine("Please enter your start point - (A-D)");
+            
+            var userStartPoint = Console.ReadLine()?.ToUpper();
+            var startPointEnum = (Core.Points)Enum.Parse(typeof(Core.Points), userStartPoint);
+
+            Console.WriteLine($"You have chosen '{userStartPoint}' as your start point.");
+            Console.WriteLine($"Please choose an end point - (A-D)");
+
+            var userEndPoint = Console.ReadLine()?.ToUpper();
+            var endPointEnum = (Core.Points)Enum.Parse(typeof(Core.Points), userEndPoint);
+
+            Console.WriteLine($"You have chosen '{userEndPoint}' as your start point.");
+
+            var shortestRouteList = new RouteFinder(paths, new RouteExplorer(), new ShortestRouteFinder()).CalculateShortestRoute(startPointEnum, endPointEnum);
+
+            //shortestRouteList.Paths.ForEach(Console.WriteLine);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"The shortest route for {userStartPoint} to {userEndPoint} is: ");
+
+            foreach (var path in shortestRouteList.Paths)
+            {
+                Console.WriteLine($"Path {path.Id}: {path.PointOne} to {path.PointTwo} - Distance {path.Distance}");
+            }
+        }
+    }
+}
